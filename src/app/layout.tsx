ee808ae143c4014edc7dc8b0a174b906c92c5b34
Lina Gromakova <linaWebDev@outlook.com>
2025-06-12 20:58:13 +0300

    Add UserContact component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,17 +5,34 @@ export const metadata: Metadata = {
   title: 'Io',
   description: 'lroem',
 };
-import { FormAuth } from '@/entities/FormAuth/form-auth-layout';
+
+import clsx from 'clsx';
+import { UserContactLayout } from '@/entities/UserContact/user-contact-layout';
 // {
 //   children,
 // }: Readonly<{
 //   children: React.ReactNode;
 // }>
+const scroll: string = ` [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]:bg-slate-800
+  hover:[&::-webkit-scrollbar-thumb]:bg-slate-400
+  [&::-webkit-scrollbar-thumb]:rounded-full`;
+const props = {
+  image: '',
+  name: 'string',
+  online: true,
+  lastMessage: 'string',
+  lastAtCreate: '17:00',
+  read: false,
+  countMessage: 0,
+};
 export default function RootLayout() {
   return (
     <html lang='en'>
       <body className=' bg-slate-900'>
-        <FormAuth></FormAuth>
+        <div className={clsx(' w-2/5 bg-slate-800 overflow-y-scroll h-screen', scroll)}>
+          <UserContactLayout {...props}></UserContactLayout>
+        </div>
       </body>
     </html>
   );
