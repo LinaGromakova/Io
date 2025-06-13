99470e27da427b938755c30bf0ffba85e69c3051
Lina Gromakova <linaWebDev@outlook.com>
2025-06-13 22:17:59 +0300

    Add entity UserMessage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ export const metadata: Metadata = {
 };
 
 import { SidebarContactsLayout } from '@/widgets/SidebarContacts/sidebar-contacts-layout';
+import { UserMessage } from '@/entities/UserMessage/user-message';
 // {
 //   children,
 // }: Readonly<{
@@ -18,7 +19,34 @@ export default function RootLayout() {
     <html lang='en'>
       <body className=' bg-slate-900 flex '>
         <SidebarContactsLayout></SidebarContactsLayout>
-        <Header type='HEADER_MAIN'></Header>
+
+        <div className='w-full'>
+          <Header type='HEADER_MAIN'></Header>
+          <div className='py-5 px-12 w-full h-[85vh] overflow-y-scroll'>
+            <div>
+              <UserMessage
+                message='Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ex quibusdam fuga cupiditate ab quasi consequatur labore, magni, accusamus repudiandae, non deserunt provident exercitationem laborum veniam optio sed? Unde, rerum.'
+                atPush='17:10'
+                read={true}
+                sender='ANOTHER'
+              ></UserMessage>
+              <UserMessage
+                message='Lorem ipsum .'
+                atPush='17:10'
+                read={true}
+                sender='ANOTHER'
+              ></UserMessage>
+            </div>
+            <div>
+              <UserMessage
+                message='Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ex quibusdam fuga cupiditate ab quasi '
+                atPush='22:10'
+                read={false}
+                sender='YOU'
+              ></UserMessage>
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
---
+++ src/entities/UserMessage/user-message.tsx
@@ -0,0 +1,48 @@
+import { JSX } from 'react';
+import { IoCheckmarkDoneOutline as MarkDoneIcon } from 'react-icons/io5';
+import { IoCheckmarkOutline as MarkOutlineIcon } from 'react-icons/io5';
+import clsx from 'clsx';
+
+interface MessageProps {
+  message: string;
+  atPush: string;
+  read: boolean;
+  sender: 'YOU' | 'ANOTHER';
+  className?: string;
+}
+
+export function UserMessage({
+  message,
+  atPush,
+  read,
+  sender,
+  className,
+}: MessageProps): JSX.Element {
+  return (
+    <article
+      className={clsx(
+        `max-w-70  min-w-8 rounded-xl text-white p-4 relative mb-3 last:before:bottom-2
+        `,
+        className,
+        sender === 'YOU'
+          ? `bg-slate-700 ml-auto last:before:content-[''] last:before:block last:before:absolute last:before:-right-6
+          last:before:border-r-transparent last:before:border-r-24 last:before:border-b-slate-400
+        last: before:border-t-transparent last:before:border-y-24 last:border-l-0`
+          : `bg-orange-400 last:before:content-[''] last:before:block last:before:absolute last:before:-left-6
+          last:before:border-l-transparent last:before:border-l-24 
+          last:before:border-b-amber-700
+        last: before:border-t-transparent last:before:border-y-24 last:border-r-0`
+      )}
+    >
+      <span className='text-md'>{message}</span>
+      <div className='absolute bottom-0.5 right-0.5 flex'>
+        <span className='text-xs text-white/30'>{atPush}</span>
+        <span className='align-text-bottom mx-1'>
+          {(read && <MarkDoneIcon></MarkDoneIcon>) || (
+            <MarkOutlineIcon className='text-white/50'></MarkOutlineIcon>
+          )}
+        </span>
+      </div>
+    </article>
+  );
+}
