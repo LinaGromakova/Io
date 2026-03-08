f353628d055720b3cda75a9df66423c6ba155eb1
Lina Nevan <linawebdeb@outlook.com>
2026-03-08 22:14:44 +0300

    Add adaptive for preloader, fix manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,46 +15,59 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning translate="no">
+    <html
+      lang='en'
+      suppressHydrationWarning
+      translate='no'
+    >
       <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/favicon/android-chrome-192x192.png"
+        <meta charSet='UTF-8' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="512x512"
-          href="/favicon/android-chrome-512x512.png"
-        />
-        <link rel="icon" href="/favicon/favicon.ico" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
+          rel='icon'
+          type='image/png'
+          sizes='192x192'
+          href='/favicon/android-chrome-192x192.png'
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          rel='icon'
+          type='image/png'
+          sizes='512x512'
+          href='/favicon/android-chrome-512x512.png'
         />
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
+          rel='icon'
+          href='/favicon/favicon.ico'
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon/favicon-16x16.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicon/apple-touch-icon.png'
+        />
+        <link
+          rel='manifest'
+          href='/favicon/site.webmanifest'
+        />
       </head>
       <body>
         <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
+          attribute='class'
+          defaultTheme='light'
           enableSystem={false}
           themes={['light', 'dark']}
         >
--- src/shared/ui/AppLoader/ui/AppLoader.tsx
+++ src/shared/ui/AppLoader/ui/AppLoader.tsx
@@ -2,8 +2,8 @@ import { Logo } from '@/shared/assets/logo';
 
 export function AppLoader() {
   return (
-    <div className="w-full h-screen bg-background flex items-center justify-center flex-col select-none">
-      <Logo className="w-1/4 h-max rounded-full logo-anim"></Logo>
+    <div className='w-full h-screen bg-background flex items-center justify-center flex-col select-none'>
+      <Logo className='w-1/4 max-lg:w-2/5 max-md:w-1/2 max-sm:w-8/12 h-max rounded-full logo-anim'></Logo>
     </div>
   );
 }
